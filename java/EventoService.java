10a28f93e7407ba716ec7e3c0051fabb29dd2d8c
wesleymou <42716264+wesleymou@users.noreply.github.com>
2019-04-27 11:18:23 -0300

    Cliente.java, ClienteService.java, Evento.java, EventoService.java
    ultimo upload da aula

--- java/EventoService.java
+++ java/EventoService.java
@@ -1,9 +1,27 @@
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.simpleframework.http.Query;
 import org.simpleframework.http.Request;
 
 class EventoService {
 	public String add(Request request) {
 		Query query = request.getQuery();
+		
+		String nome = query.get("nome");
+//		LocalDateTime dataInicio = query.get(key);
+//		LocalDateTime dataTermino = query.get(key);
+		String local = query.get("local");
+		int capacidade = query.getInteger("capacidade");
+		int quorum = query.getInteger("quorum");
+		double orcamentoPrevio = query.getFloat("orcamentoPrevio");
+		double valorIngresso = query.getFloat("valorIngresso");
+//		Map <Array,Array> cronograma = new HashMap<Array,Array>() = query.get(key);
+//		int convenio[] = query.get(key);
+//		Inscricoes = new Inscricao[] = query.get(key);
+		String status = query.get("status");
 		
 		return null;
 	}
