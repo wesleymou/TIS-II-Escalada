76fcff9110eee278d29aedd8eb80e5e4f1c90afe
Gavisira <42875966+Gavisira@users.noreply.github.com>
2019-05-01 11:29:58 -0300

    Inscricao.java,ListaClientes.java,CelulaCliente.java
    Classes Java

---
+++ java/ListaClientes.java
@@ -0,0 +1,24 @@
+
+public class ListaClientes {
+private CelulaCliente primeiro;
+private CelulaCliente ultimo;
+
+public ListaClientes(){
+	
+}
+public void inserirFinal(Cliente cliente) {
+	
+}
+public void retirar(Cliente cliente) {
+	
+}
+public void localizar (int[] cpf) {
+	
+}
+public boolean listaVazia() {
+	
+}
+public void imprimir() {
+	
+}
+}
