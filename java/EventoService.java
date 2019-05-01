7762f07e4d82a95f75e39291501435498574d2c9
wesleymou <42716264+wesleymou@users.noreply.github.com>
2019-05-01 02:41:55 -0300

    metodos add, get e remove

--- java/EventoService.java
+++ java/EventoService.java
@@ -3,49 +3,61 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.simpleframework.http.Query;
 import org.simpleframework.http.Request;
 
-class EventoService {
+public final class EventoService {
+	private ListaEvento listaDeEventos;
+
 	public String add(Request request) {
 		Query query = request.getQuery();
-		
+
 		String nome = query.get("nome");
-//		LocalDateTime dataInicio = query.get(key);
-//		LocalDateTime dataTermino = query.get(key);
+		//		LocalDateTime dataInicio = query.get(key);
+		//		LocalDateTime dataTermino = query.get(key);
 		String local = query.get("local");
 		int capacidade = query.getInteger("capacidade");
 		int quorum = query.getInteger("quorum");
 		double orcamentoPrevio = query.getFloat("orcamentoPrevio");
 		double valorIngresso = query.getFloat("valorIngresso");
-//		Map <Array,Array> cronograma = new HashMap<Array,Array>() = query.get(key);
-//		int convenio[] = query.get(key);
-//		Inscricoes = new Inscricao[] = query.get(key);
+		//		Map <Array,Array> cronograma = new HashMap<Array,Array>() = query.get(key);
+		//		int convenio[] = query.get(key);
+		//		Inscricoes = new Inscricao[] = query.get(key);
 		String status = query.get("status");
-		
+
+		Evento evento = new Evento(nome, local, dataInicio, dataTermino);
+		evento.setCapacidade(capacidade);
+		evento.setQuorum(quorum);
+		evento.setOrcamentoPrevio(orcamentoPrevio);
+		evento.setValorIngresso(valorIngresso);
+		evento.setCronograma(cronograma);
+		evento.setConvenio(convenio);
+		evento.setStatus(status);
+
+		listaDeEventos.create(evento);
+		return evento.toString();
+	}
+
+	public JSONObject get(Request request) {
+		Evento evento = listaDeEventos.read(request.getQuery().get("nome"));
+		if ((evento) != null)
+			return evento.toJson();
 		return null;
 	}
-	
-	public String get(Request request) {
-		Query query = request.getQuery();
-		String id = query.get("");
-		
-		return null;
-		
-	}
-	
+
+	//TODO - Terminar o metodo de update
 	public String update(Request request) {
-		Query query = request.getQuery();
-		
+		String nome = request.getQuery().get("nome");
+		listaDeEventos.update(nome);
 		return null;
-		
+
 	}
-	
-	public String remove (Request request) {
-		Query query = request.getQuery();
-		String id = query.get("");
-		
+
+	public JSONObject remove (Request request) {
+		Evento evento = listaDeEventos.delete(request.getQuery().get("nome"));
+		if (evento != null)
+			return evento.toJson();
 		return null;
-		
 	}
 }
