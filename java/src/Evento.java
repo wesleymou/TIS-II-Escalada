ac041a0c1416014ac5b3487b3954e9a7c5160248
Paulo <padbarbosa87@gmail.com>
2019-05-04 23:56:54 -0300

    css meia boca
    + pequenas mudanças

--- java/src/Evento.java
+++ java/src/Evento.java
@@ -45,66 +45,87 @@ public class Evento {
 		public String getNome() {
 			return nome;
 		}
+		
 		public void setNome(String nome) {
 			this.nome = nome;
 		}
+		
 		public LocalDateTime getDataInicio() {
 			return dataInicio;
 		}
+		
 		public void setDataInicio(LocalDateTime dataInicio) {
 			this.dataInicio = dataInicio;
 		}
+		
 		public LocalDateTime getDataTermino() {
 			return dataTermino;
 		}
+		
 		public void setDataTermino(LocalDateTime dataTermino) {
 			this.dataTermino = dataTermino;
 		}
+		
 		public String getLocal() {
 			return local;
 		}
+		
 		public void setLocal(String local) {
 			this.local = local;
 		}
+		
 		public int getCapacidade() {
 			return capacidade;
 		}
+		
 		public void setCapacidade(int capacidade) {
 			this.capacidade = capacidade;
 		}
+		
 		public int getQuorum() {
 			return quorum;
 		}
+		
 		public void setQuorum(int quorum) {
 			this.quorum = quorum;
 		}
+		
 		public double getOrcamentoPrevio() {
 			return orcamentoPrevio;
 		}
+		
 		public void setOrcamentoPrevio(double orcamentoPrevio) {
 			this.orcamentoPrevio = orcamentoPrevio;
 		}
+		
 		public double getValorIngresso() {
 			return valorIngresso;
 		}
+		
 		public void setValorIngresso(double valorIngresso) {
 			this.valorIngresso = valorIngresso;
 		}
+		
 		public int[] getConvenio() {
 			return convenio;
 		}
+		
 		public void setConvenio(int[] convenio) {
 			this.convenio = convenio;
 		}
+		
 		public String getStatus() {
 			return status;
 		}
+		
 		public void setStatus(String status) {
 			this.status = status;
 		}
+		
 		public Map <Array,Array> getCronograma() {
 			return cronograma;
 		}
+		
 		public void setCronograma(Map <Array,Array> cronograma) {
 			this.cronograma = cronograma;
 		}
